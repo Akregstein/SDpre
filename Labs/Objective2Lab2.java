5c499e672138a41400eed1f187bc67e6b78dc628
Daniel.Kregstein <Akregstein@hotmail.com>
2022-02-05 22:42:55 -0700

    complete Objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Daniel Kregstein";
+    int age = 36;
+    String state = "Colorado";
+
+    System.out.println("Name:"+name +"Age:"+ age + "State:" + state);
+  }
+}
