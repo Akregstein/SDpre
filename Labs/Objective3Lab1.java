93ef508d741fa686420f67cb7d7b779012042344
Daniel.Kregstein <Akregstein@hotmail.com>
2022-02-06 00:53:05 -0700

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,11 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1 = 32.2;
+    double num2 = 44.3;
+    double num3 = 54.6;
+    double average =(num1 + num2 + num3) / 3;
+
+    System.out.println(average);
+
+  }
+}
