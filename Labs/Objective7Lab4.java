5ae3083056d08c34f188fce7019973c4aea39da7
Daniel.Kregstein <Akregstein@hotmail.com>
2022-02-10 22:35:43 -0700

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,16 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int counter = 0;
+    int sum = 0;
+
+
+    while(counter <= 20) {
+      System.out.println(sum);
+      counter ++;
+      sum = sum + counter;
+
+
+
+    }
+  }
+}
