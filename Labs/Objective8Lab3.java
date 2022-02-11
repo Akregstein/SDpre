c171dca33a51eb07766c51b794f34418de1d447d
Daniel.Kregstein <Akregstein@hotmail.com>
2022-02-11 02:56:37 -0700

    complete objective8lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,17 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+
+
+    for(int i = 1; i<21; i++){
+
+      if(i % 2 == 0){
+        System.out.println(i +" is even");
+      }
+      if(i % 2 == 1){
+        System.out.println(i +" is odd");
+      }
+
+
+    }
+  }
+}
